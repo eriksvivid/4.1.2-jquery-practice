a2ca6ccc134bd713bea6eb86753dff8c7ccf0dc3
Erik <sanchezerik78@yahoo.com>
2016-04-07 12:19:40 -0400

    working

---
+++ app/scripts/index.js
@@ -0,0 +1,13 @@
+var $ = require('jquery');
+
+$(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
+
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+
+      //Hide the other panels
+      $(".accordion-content").not($(this).next()).slideUp('fast');
+
+    });
+  });
